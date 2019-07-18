992070c027c5a4910ecc7342ee812d393600c17b
Victor Gil <baskeboler@gmail.com>
2019-07-18 18:21:32 -0300

    added logo to page loader

--- src/main/cljs_karaoke/views/page_loader.cljs
+++ src/main/cljs_karaoke/views/page_loader.cljs
@@ -1,12 +1,23 @@
 (ns cljs-karaoke.views.page-loader
   (:require [re-frame.core :as rf]
             [cljs-karaoke.events :as events]
-            [cljs-karaoke.subs :as subscriptions]))
+            [cljs-karaoke.subs :as subscriptions]
+            [stylefy.core :as stylefy]))
 
+(def loader-logo
+  {:display :block
+   :position :absolute
+   :height "60vh"
+   :width "60vw"
+   :top "50%"
+   :left "50%"
+   :transform "translate(-50%, -50%)"})
 (defn page-loader-component []
   [:div.pageloader
    {:class (if @(rf/subscribe [::subscriptions/pageloader-active?])
              ["is-active"]
              [])}
-   [:span.title
-    "Loading Karaoke"]])
+   [:img (stylefy/use-style
+          loader-logo
+          {:src "/images/logo.svg"
+           :alt ""})]])
